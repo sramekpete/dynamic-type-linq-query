28b970b632e57a30ddaa7d2404b30f743e66fa29
Petr Šrámek <petr.sramek@outlook.cz>
2019-08-11 19:24:22 +0200

    Fully working sample of custom dynamic behavior added to declared class in runtime

---
+++ src/DropoutCoder.DynamicTypeLinqQuery/Data/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Dynamic;
+using System.Linq.Expressions;
+using DropoutCoder.DynamicTypeLinqQuery.Data.Meta;
+
+namespace DropoutCoder.DynamicTypeLinqQuery.Data {
+    public sealed class Person : IDynamicMetaObjectProvider {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public int GetAge() {
+            return DateTime.Today.Year - BirthDate.Year;
+        }
+
+        DynamicMetaObject IDynamicMetaObjectProvider.GetMetaObject(Expression parameter) {
+            return new PersonMetaObject(parameter, this);
+        }
+    }
+}
---
+++ src/DropoutCoder.DynamicTypeLinqQuery/Expressions/ExpressionFactory.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq.Expressions;
+
+using Microsoft.CSharp.RuntimeBinder;
+
+namespace DropoutCoder.DynamicTypeLinqQuery.Expressions {
+    public static class ExpressionFactory {
+        public static Expression<Func<dynamic, bool>> PropertyGreaterThanPredicate(string propertyName, int value){
+            var constant = Expression.Constant(value);
+
+            var parameter = Expression.Parameter(typeof(object), "m");
+
+            // Creating binder to access class member
+            var binder = Binder.GetMember(CSharpBinderFlags.None, propertyName, typeof(Program), new CSharpArgumentInfo[] { CSharpArgumentInfo.Create(CSharpArgumentInfoFlags.None, null) });
+
+            // Dynamic operation on the binder
+            var dynamic = Expression.Dynamic(binder, typeof(object), parameter);
+
+            // Converting result to same type as value to compare
+            var converted = Expression.Convert(dynamic, constant.Value.GetType());
+
+            var predicate = Expression.GreaterThan(converted, constant);
+
+            var lambda = Expression.Lambda<Func<dynamic, bool>>(predicate, parameter);
+
+            return lambda;
+        }
+
+        public static Expression<Func<dynamic, bool>> IndexLessThanPredicate(string indexName, int value) {
+            var constant = Expression.Constant(value);
+
+            var parameter = Expression.Parameter(typeof(object), "m");
+
+            // Creating binder to access indexer
+            var binder = Binder.GetIndex(CSharpBinderFlags.None, typeof(Program), new CSharpArgumentInfo[] { CSharpArgumentInfo.Create(CSharpArgumentInfoFlags.None, null) });
+
+            // Dynamic operation on the binder
+            var dynamic = Expression.Dynamic(binder, typeof(object), parameter, Expression.Constant(indexName));
+
+            // Converting result to same type as value to compare
+            var converted = Expression.Convert(dynamic, constant.Value.GetType());
+
+            var predicate = Expression.LessThan(converted, constant);
+
+            var lambda = Expression.Lambda<Func<dynamic, bool>>(predicate, parameter);
+
+            return lambda;
+        }
+    }
+}
